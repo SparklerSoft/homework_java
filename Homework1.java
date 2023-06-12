5cb7bb38d62edf0d6d7f356375cbcf15993cc5c3
Sparkler Soft <sparklersoft@gmail.com>
2023-06-12 18:55:31 +0200

    Add files via upload
     1 уровень сложности: напишите код, который при запуске выведет в консоль:
    
    /'"работу выполнил/а ваше_имя"'\
    
    (вывести спец символы в консоль - обязательное условие, посмотрите дополнительно тему "экранирование символов")
    
    создайте 8 переменных для всех 8 примитивных типов данных, распечатайте значение каждой,
    затем распечатайте пустую строку, чтобы отделить следующий вывод в консоль от предыдущего,
    измените значения ваших переменных, не создавая новых и распечатайте их вновь.

---
+++ Homework1.java
@@ -0,0 +1,46 @@
+public class Homework1 {
+
+    public static void main(String[] args){
+
+
+    System.out.println("/\'\"Work was created/by Serg_Lapidus\"\'\\");
+
+    boolean a = false;
+    System.out.println(a);
+
+    byte i = 122;
+    System.out.println(i);
+
+    short b = 30_790;
+    System.out.println(b);
+
+    int c = 2_000_110_000;
+    System.out.println(c);
+
+    long s = 34_999_456_333_556_344L;
+    System.out.println(s);
+
+    float w = 0.834_399_544_233f;
+    System.out.println(w);
+
+    double t = 0.967_234_567_398_432_551_444d;
+    System.out.println(t);
+
+    char R = (char) 04234122D;
+    System.out.println(R);
+
+    System.out.println("\n"); // Добавить и распечатать пустую строку
+
+    System.out.println("Check the new line\n"); // Проверка вставки пустой строки
+
+
+
+
+
+
+
+
+
+
+    }
+}
