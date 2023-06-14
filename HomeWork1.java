ac5fa52180b99445f2368ac93fba178975abfe7b
Sparkler Soft <sparklersoft@gmail.com>
2023-06-14 09:26:59 +0200

    Add files via upload
    Variables names are changed...
    Variables values are changed and printed...

--- HomeWork1.java
+++ HomeWork1.java
@@ -4,42 +4,50 @@ public class HomeWork1 {
 
     System.out.println("/\'\"Work was created/by Serg_Lapidus\"\'\\");
 
-    boolean a = false;
-    System.out.println(a);
+    boolean myBoolean = false;
+    System.out.println(myBoolean);
 
-    byte i = 122;
-    System.out.println(i);
+    byte myByte = 122;
+    System.out.println(myByte);
 
-    short b = 30_790;
-    System.out.println(b);
+    short myShort = 30_790;
+    System.out.println(myShort);
 
-    int c = 2_000_110_000;
-    System.out.println(c);
+    int myInt = 2_000_110_000;
+    System.out.println(myInt);
 
-    long s = 34_999_456_333_556_344L;
-    System.out.println(s);
+    long myLong = 34_999_456_333_556_344L;
+    System.out.println(myLong);
 
-    float w = 0.834_399_544_233f;
-    System.out.println(w);
+    float myFloat = 0.834_399_544_233f;
+    System.out.println(myFloat);
 
-    double t = 0.967_234_567_398_432_551_444d;
-    System.out.println(t);
+    double myDouble = 0.967_234_567_398_432_551_444d;
+    System.out.println(myDouble);
 
-    char R = (char) 04234122D;
-    System.out.println(R);
+    char myChar = (char) 04234122D;
+    System.out.println(myChar);
 
     System.out.println("\n"); // Add and print an empty string
 
-    System.out.println("Check the new line\n"); // Checking the empty string
+    System.out.println("Check the new line\n"); // Check the empty string adding
 
-
-
-
-
-
-
-
-
+    myBoolean = true;
+    myByte = 118;
+    myShort = 26_889;
+    myInt = 2_000_330_000;
+    myLong = 34_999_456_567_556_344L;
+    myFloat = 0.711_111_544_298f;
+    myDouble = 0.888_777_911_398_432_551_444d;
+    myChar = 0332;
+    System.out.println(myBoolean);
+    System.out.println(myByte);
+    System.out.println(myShort);
+    System.out.println(myInt);
+    System.out.println(myLong);
+    System.out.println(myFloat);
+    System.out.println(myDouble);
+    System.out.println(myChar);
 
     }
 }
