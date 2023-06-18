0f8584a27e9d3197cbab25775ad778bc51dae027
Sparkler Soft <sparklersoft@gmail.com>
2023-06-18 22:14:58 +0200

    Add files via upload

--- HomeWork2.java
+++ HomeWork2.java
@@ -22,53 +22,47 @@ public class HomeWork2 {
     private static void tryScanner() {
         Scanner inputParcelsAmount = new Scanner(System.in); //Input your parcels amount
         System.out.println("Please, input your parcels amount");
-
         int amountParcels = inputParcelsAmount.nextInt(); // User have to input parcels amount
-
-        int parcels = amountParcels;
 
         Scanner inputParcelsWeight = new Scanner(System.in); //Input your parcels weight
         System.out.println("Please, input one parcel weight in kilo");
         float weightParcels = inputParcelsWeight.nextFloat(); //User have to input parcels amount
-        float weight = weightParcels;
 
         float weightParcelsTotal = amountParcels * weightParcels; //Total parcel's weight
         System.out.println("Total weight of your parcel is " + weightParcelsTotal + " kilo");
 
         Scanner inputReceiver = new Scanner(System.in); //Input receiver's address
-        System.out.println("Please, input receiver's address");
+        System.out.println("Please, input Receiver's address");
         String inputReceiverAddress = inputReceiver.nextLine();
-        System.out.println("Your sender's address is: " + inputReceiverAddress.toUpperCase());
+        System.out.println("Your Receiver's address is: " + inputReceiverAddress.toUpperCase());
 
-
-        Scanner inputUsersName = new Scanner(System.in); //Input your name
-        System.out.println("Please, input your name");
-
+        Scanner inputUsersName = new Scanner(System.in); //Input your receiver name
+        System.out.println("Please, input Receiver's first name");
         String inputUsersNameString = inputUsersName.next();
 
         //String firstNameLetter = inputUsersNameString.substring(0,1);
         //String endNameLetter = inputUsersNameString.substring(1);
         //String newUserName = inputUsersNameString.substring(0,1).toUpperCase() + inputUsersNameString.substring(1);
+        System.out.println("Your Receiver's name is: " + inputUsersNameString.toUpperCase()); //Receiver's name review
 
-
-
-
-        System.out.println("Your sender's name is: " + inputUsersNameString.toUpperCase()); //Sender's name review
-
-        Scanner inputUsersLastName = new Scanner(System.in); //Input your last name
-        System.out.println("Please, input your last name");
-
+        Scanner inputUsersLastName = new Scanner(System.in); //Input your receiver's last name
+        System.out.println("Please, input Receiver's last name");
         String inputUsersLastNameString = inputUsersLastName.next();
 
         //String firstLastNameLetter = inputUsersLastNameString.substring(0,1);
         //String endLastNameLetter = inputUsersLastNameString.substring(1);
         //String newUserLastName = inputUsersLastNameString.substring(0,1).toUpperCase() + inputUsersLastNameString.substring(1);
-
         System.out.println("Your sender's Last name is: " + inputUsersLastNameString.toUpperCase());//Sender's Last name review
+
+        System.out.println("\"Receiver: " + inputUsersNameString.toUpperCase() + " " + inputUsersLastNameString.toUpperCase() + "\"");
+        System.out.println("\"You send: " + amountParcels + " parcels " + " with total weight " + weightParcelsTotal + " kilo\"");
+
+        /*
         System.out.println("You send " + amountParcels + " parcels");
         System.out.println("Your total parcel's weight is: " + weightParcelsTotal + " kilo");
         System.out.println("Sender name is: " + inputUsersNameString.toUpperCase());
         System.out.println("Sender Last name is: " + inputUsersLastNameString.toUpperCase());
+        */
 
         Scanner inputCheckBox = new Scanner(System.in); //Checkbox yes/no
         System.out.println("All data correct?" + " Please input \"Yes\" or \"No\"");
